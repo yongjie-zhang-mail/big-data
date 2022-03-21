507aef3275203b42b64995d5d1af544f4fcd84ea
yongjie <yongjie_zhang_mail@qq.com>
2022-03-21 19:50:05 +0800

    打包

--- src/main/scala/com/yj/bigdata/test/Test.scala
+++ src/main/scala/com/yj/bigdata/test/Test.scala
@@ -8,13 +8,30 @@ import org.apache.spark.sql.SparkSession
 object Test {
     
     def main(args: Array[String]): Unit = {
-        
+    
         val spark = SparkSession
             .builder
+            .master("local")
             .appName("HdfsTest")
             .getOrCreate()
-        
-        
+    
+        val filePath = args(0)
+        //        val filePath = "./data/README.md"
+        //        val rdd = spark.sparkContext.textFile(filePath)
+        //        val lines = rdd.flatMap(x => x.split(" "))
+        //            .map(x => (x, 1))
+        //            .reduceByKey((a, b) => (a + b))
+        //            .collect().toList
+        //        println(lines)
+    
+        import spark.implicits._
+        val ds = spark.read.textFile(filePath)
+            .flatMap(x => x.split(" "))
+            .map(x => (x, 1))
+            .groupBy("_1")
+            .count()
+            .show()
+    
     }
     
 }
